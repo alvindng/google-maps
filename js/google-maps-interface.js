5371400d4149474fa94d841095c652b306c2264b
Alvin Ng and Ben Logue <ben@>
2016-08-04 09:37:07 -0700

    refactor functions to separate front-end and backend.

--- js/google-maps-interface.js
+++ js/google-maps-interface.js
@@ -1,57 +1,41 @@
 var Google = require('./../js/google-maps.js').googleModule;
 var Weather = require('./../js/google-maps.js').weatherModule;
 
-var breweryKey = require('./../.env').breweryKey;
-var gmapsKey = require('./../.env').gmapsKey;
-var weatherKey = require('./../.env').weatherKey;
-var instagramIdKey = require('./../.env').instagramIdKey;
-var instagramSecretKey = require('./../.env').instagramSecretKey;
 
+
+var displayWeather = function(location, weatherData) {
+  $('#temperature').html("The humidity in " + location + " is " + weatherData[0] + "%.");
+  $('#fahrenheit').html("The temperature in " + location + " is " + weatherData[1] + "&#x2109.");
+  $('#celsius').html("The temperature in " + location + " is " + weatherData[2] + "&#x2103.");
+};
+
+var displayDefault = function(gmapsKey) {
+  $("#map").html('<iframe width="600" height="450" frameborder="0" src="https://www.google.com/maps/embed/v1/place?key='+ gmapsKey +
+  '&q=epicodus"></iframe>');
+};
+
+var displaySearch = function(gmapsKey, location) {
+  $("#map").html('<iframe width="600" height="450" frameborder="0" src="https://www.google.com/maps/embed/v1/place?key='+ gmapsKey +
+  '&q=' + location + '"></iframe>');
+
+};
 
 $(document).ready(function() {
   var coord = [];
+  var newGoogle = new Google();
+  newGoogle.getDefault(displayDefault);
 
   $('#city').submit(function(event) {
     event.preventDefault();
     var location = $("#location").val();
     $('#location').val('');
 
+    newGoogle.getWeather(location, displayWeather);
+    newGoogle.getSearch(location, displaySearch);
 
-    $.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + gmapsKey).then(function(response) {
-      var city = response.results[0].address_components[0].long_name;
-      var lat = response.results[0].geometry.location.lat;
-      var long = response.results[0].geometry.location.lng;
-      var address = response.results[0].formatted_address;
-      coord[0] = lat;
-      coord[1] = long;
-      console.log(response);
-      console.log(coord);
-    }).then(function(){
-      $.get('http://api.openweathermap.org/data/2.5/weather?lat=' + coord[0] + '&lon=' + coord[1] + '&appid=' + weatherKey).then(function(response) {
-        console.log(response);
-        var temp = new Weather(response.main.temp);
-        $('#temperature').html("The humidity in " + location + " is " + response.main.humidity + "%.");
-        $('#fahrenheit').html("The temperature in " + location + " is " + temp.fahrenheit + "&#x2109.");
-        $('#celsius').html("The temperature in " + location + " is " + temp.celsius + "&#x2103.");
-      }).then(function(response){
-        $.get("http://api.brewerydb.com/v2/?key=" + breweryKey).then(function(response) {
-          console.log(response);
-        });
-      }).fail(function(error) {
-        $('#temperature').text(error.responseJSON.message);
-      });
-    });
-
-
-
-
-    $("#map").html('<iframe width="600" height="450" frameborder="0" src="https://www.google.com/maps/embed/v1/place?key='+ gmapsKey +
-    '&q='+location+ '"></iframe>');
 
   });
 
-  $("#map").html('<iframe width="600" height="450" frameborder="0" src="https://www.google.com/maps/embed/v1/place?key='+ gmapsKey +
-  '&q=epicodus"></iframe>');
 
 
 
--- js/google-maps.js
+++ js/google-maps.js
@@ -1,14 +1,49 @@
 var gmapsKey = require('./../.env').gmapsKey;
 var weatherKey = require('./../.env').weatherKey;
 
-function Google() {
+var coord = [];
 
+function Google() {
 }
 
 function Weather(kelvin) {
   this.celsius = (kelvin - 273.15).toFixed(2);
   this.fahrenheit = ((kelvin * (9/5)) - 459.67).toFixed(2);
 }
+
+Google.prototype.getDefault = function(displayDefault) {
+    displayDefault(gmapsKey);
+};
+
+Google.prototype.getSearch = function(location, displaySearch) {
+  displaySearch(gmapsKey, location);
+};
+
+Google.prototype.getWeather = function(location, displayWeather) {
+  $.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + gmapsKey).then(function(response) {
+    var city = response.results[0].address_components[0].long_name;
+    var lat = response.results[0].geometry.location.lat;
+    var long = response.results[0].geometry.location.lng;
+    var address = response.results[0].formatted_address;
+    coord[0] = lat;
+    coord[1] = long;
+    console.log(response);
+    console.log(coord);
+  }).then(function(){
+    $.get('http://api.openweathermap.org/data/2.5/weather?lat=' + coord[0] + '&lon=' + coord[1] + '&appid=' + weatherKey).then(function(response) {
+      console.log(response);
+      var weatherData = [];
+      var temp = new Weather(response.main.temp);
+      weatherData[0] = response.main.humidity;
+      weatherData[1] = temp.fahrenheit;
+      weatherData[2] = temp.celsius;
+      displayWeather(location, weatherData);
+    }).fail(function(error) {
+      $('#temperature').text(error.responseJSON.message);
+    });
+  });
+};
+
 
 exports.googleModule = Google;
 exports.weatherModule = Weather;
